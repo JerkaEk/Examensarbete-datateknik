5c3f486dccf8bde7f04478e2c4bf0898a7acf5de
wsjoden <sjoden.william@gmail.com>
2026-04-08 13:31:21 +0200

    simple controller as a first step to move into MVC achitecture

--- core/Controller.py
+++ core/Controller.py
@@ -1,13 +1,12 @@
 import logging
 
-from calibration import calibration, auto_settings
-from pose_estimaiton import pose2d_extractor, triangulate3d #?
-from gui import gui
+#from calibration import calibration, auto_settings
+#from pose_estimaiton import pose2d_extractor, triangulate3d #?
 
 log = logging.getLogger(__name__)
 
 class Controller:
-  def __init__(self):
+  def __init__(self, no_gui=False, mock_cameras=False):
       
     self.no_gui = no_gui
     self.mock_cameras = mock_cameras
@@ -16,8 +15,7 @@ class Controller:
     self.preview_active = False
     self.gui = None
     self.calibration_running = False
-   
-    
+  
   def run(self):
     if not self.no_gui:
       self._start_gui()
@@ -31,12 +29,23 @@ class Controller:
     # Close all process threads here.    
       
 
-  def on_calibration_click(self):
+  def on_calibrate_clicked(self):
     if self.calibration_running:
-      log.warning("Calibration already running")
-      return
+        log.warning("Calibration already running")
+        return
+
     log.info("Starting calibration")
     self.calibration_running = True
+
+    from calibration.calibration import run_calibration
+    success = run_calibration("calibration/calibration_settings.yaml")
+
+    if success:
+        log.info("Calibration complete")
+    else:
+        log.error("Calibration failed")
+
+    self.calibration_running = False
     
   def on_calibration_settings_clicked(self):
     log.info("Opening calibration settings")
@@ -55,17 +64,11 @@ class Controller:
   
   # Send to GUI
   
-  
-    
-    
   # Utilities
-  def _start_gui(self):
-    # TODO Create MainWindow
-    log.info("_start_gui() - stub")
     
   def _start_preview(self):
     # TODO implement into gui
-    
+    pass
     self.preview_active = True
     log.info("Preview started (stub)")
     
@@ -74,5 +77,10 @@ class Controller:
     
     self.preview_active = False
     log.info("Preview stopped (stub)")
+    
+  def _start_gui(self):
+    from gui.gui_new import MainWindow
+    self.gui = MainWindow(controller=self)
+    self.gui.run()
       
   
